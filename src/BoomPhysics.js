d0ebb67a3be546c98bcf3a64ccb0834d5cce79e1
Shawn Lee <shawnsmlee@gmail.com>
2014-08-25 21:05:41 -0700

    initial commit

---
+++ src/BoomPhysics.js
@@ -0,0 +1,14 @@
+var PhysicsEngine = function( canvasID ) {
+	this.canvas = document.getElementByID(canvasID);
+	this.gravity = 9.81;
+	
+	this.setGravity(g) {
+		this.gravity = g;
+	}
+
+	
+};
+
+var PhysicsObj = function() {
+	this.
+}
